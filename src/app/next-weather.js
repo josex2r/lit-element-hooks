a0ed036a9cd35ff4e121f80ab7ac11849f88fc7f
Jose Luis Represa <joseluis.represa@bbva.com>
2020-02-19 15:26:18 +0100

    backup

--- src/app/next-weather.js
+++ src/app/next-weather.js
@@ -1,24 +1,51 @@
+
 import { css, html } from 'lit-element';
 import { component } from 'haunted-lit-element';
-import { asyncReplace } from 'lit-html/directives/async-replace.js';
+import { useState } from 'haunted';
+import { until } from 'lit-html/directives/until.js';
 
-const wait = (delay) => new Promise(res => setTimeout(res, delay));
+const VALID_UNITS = ['C', 'F'];
+const GEOCODER_KEY = '460639f5150140d098b337d93cefb8a5';
+const WEATHER_KEY = '845e19fd682309946e6a00a4936273cd';
 
-async function *counter(index = 0) {
-  while(true) {
-    await wait(1000)
-    yield index;
-    index++;
+const getRndTemp = (min = -10, max = 40) => Math.floor(Math.random() * (+max - min)) + min;
+
+const getCityCoords = (city) =>
+  fetch(`https://api.opencagedata.com/geocode/v1/json?key=${GEOCODER_KEY}&q=${city},es&pretty=1&no_annotations=1`)
+    .then(res => res.json())
+    .then(data => data.results[0].geometry);
+
+const getCoordsTemperature = (lat, lng) =>
+  fetch(`https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${WEATHER_KEY}`)
+    .then(res => res.json())
+    .then(data => data.main.temp);
+
+const getCityTemperature = (city) =>
+  getCityCoords(city)
+  .then(({ lat, lng }) => getCoordsTemperature(lat, lng));
+
+const NextWeather = ({ temperature = 10, unit = 'C', city = 'Madrid' }) => {
+  const [prevUnit, changeUnit] = useState(unit);
+  const [temp, changeTemperature] = useState(temperature);
+
+  if (!VALID_UNITS.includes(unit)) {
+     unit = prevUnit;
+  } else {
+    changeUnit(unit)
   }
-}
 
-const AsyncIterator = () => {
   return html`
-    <h1>Counter: ${asyncReplace(counter())}</h1>
+      <h1>${until(getCityTemperature(city), temp)}º ${unit}</h1>
+      <h3>Ahora, en ${city}.</h3>
+      <button @click=${() => changeTemperature(getRndTemp())}>Actualizar</button>
   `;
 }
 
-const properties = {};
+const properties = {
+  temperature: {type: Number},
+  unit: {type: String},
+  city: {type: String}
+};
 const styles = css`
   :host {
     display: block;
@@ -29,4 +56,4 @@ const styles = css`
 `;
 const options = { properties, styles };
 
-customElements.define('async-iterator', component(AsyncIterator, options));
+customElements.define('next-weather', component(NextWeather, options));
